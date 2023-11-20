e4c166da4fd7b4d60172dfcc32b1e8ad3621d4ab
Ninjagor <88942472+Ninjagor@users.noreply.github.com>
2023-11-19 21:13:23 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,36 +3,49 @@ Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineKeyHandler -Chord Tab -Function Complete
 Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Insert
 Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Command
+
 function prompt {
     $arrow = [char]0x2192  # Unicode right arrow character
     $currentDirectory = (Get-Location).Path | Split-Path -Leaf
 
     $gitBranch = ""
     $gitDirectory = (Get-Command git -ErrorAction SilentlyContinue).Path
+    $gitChanges = $null
+
     if ($gitDirectory) {
         $gitStatus = & git rev-parse --is-inside-work-tree 2>$null
         if ($gitStatus -eq 'true') {
             $gitBranch = & git symbolic-ref --short HEAD 2>$null
+            $gitChanges = & git status --short 2>$null
         }
     }
-
-
 
     $arrowColor = 'Green'
     $directoryColor = 'Cyan'
     $gitLabelColor = 'Blue'
     $gitBranchColor = 'Red'
+    $changesColor = 'Yellow'
+
+    $xicon = [char]0x2718  # Unicode character for X
 
     Write-Host -NoNewline "$arrow " -ForegroundColor $arrowColor
     Write-Host -NoNewline "$currentDirectory" -ForegroundColor $directoryColor
     if ($gitBranch) {
         Write-Host -NoNewline " git:(" -ForegroundColor $gitLabelColor
         Write-Host -NoNewline "$gitBranch" -ForegroundColor $gitBranchColor
-        Write-Host -NoNewline ")" -ForegroundColor $gitLabelColor
+
+        if ($gitChanges) {
+            Write-Host -NoNewline ")" -ForegroundColor $gitLabelColor
+            Write-Host -NoNewline " " -ForegroundColor $changesColor
+            Write-Host -NoNewline "$xicon" -ForegroundColor $changesColor
+        } else {
+            Write-Host -NoNewline ")" -ForegroundColor $gitLabelColor
+        }
     }
 
     return ' '  # Prevents "PS>"
 }
+
 
 function touch {
     param(
